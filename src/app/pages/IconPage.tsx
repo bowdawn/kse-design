7dede0dfdda145c0841c4490e184ac5edde7720a
Oleinikov, Bogdan Konstantinovich <boleinikov@gmail.com>
2023-02-01 13:03:11 +0900

    no message

--- src/app/pages/IconPage.tsx
+++ src/app/pages/IconPage.tsx
@@ -3,6 +3,7 @@ import SearchOutlined from "../../core/svg/search-outlined.svg";
 import SafeboxOutlined from "../../core/svg/safebox-outlined.svg";
 import MenuOutlined from "../../core/svg/menu-outlined.svg";
 import CloseOutlined from "../../core/svg/close-outlined.svg";
+import HeartPlusOutlined from "../../core/svg/heart-plus-outlined.svg";
 
 export const IconPage: React.FC = () => {
   return (
@@ -11,7 +12,6 @@ export const IconPage: React.FC = () => {
       <SearchOutlined className="kse-icon-small" />
       <div>Default Icon (size 32px)</div>
       <MenuOutlined className="kse-icon" />
-
       <h1>Current Assets</h1>
       <div className="kse-space kse-space-middle">
         <div>
@@ -32,7 +32,8 @@ export const IconPage: React.FC = () => {
           <CloseOutlined className="kse-icon" />
         </div>
         <div>
-          <div>icon 5</div>( - )
+          <div>Heart Plus</div>
+          <HeartPlusOutlined className="kse-icon" />
         </div>
         <div>
           <div>icon 6</div>( - )
--- src/app/pages/InputPage.tsx
+++ src/app/pages/InputPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import SearchOutlined from "../../core/svg/search-outlined.svg";
 
 export const IconInput = (
   <div className="kse-input-suffix-wrapper">
     <input className="kse-input" />
-    <button className="kse-input-btn-suffix kse-btn kse-btn-default">
-      <svg viewBox="64 64 896 896" className="kse-icon-small">
-        <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-      </svg>
-    </button>
+    <div>
+      <button className="kse-input-btn-suffix kse-btn kse-btn-default">
+        <SearchOutlined className="kse-icon-small" />
+      </button>
+    </div>
   </div>
 );
 
@@ -21,11 +22,11 @@ export const InputPage: React.FC = () => {
       <div>icon input (wrapper)</div>
       <div className="kse-input-suffix-wrapper kse-input-wrapper">
         <input />
-        <button className="kse-input-btn-suffix kse-btn kse-btn-default">
-          <svg viewBox="64 64 896 896" className="kse-icon-small">
-            <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-          </svg>
-        </button>
+        <a>
+          <button className="kse-input-btn-suffix kse-btn kse-btn-default">
+            <SearchOutlined className="kse-icon-small" />
+          </button>
+        </a>
       </div>
     </>
   );
