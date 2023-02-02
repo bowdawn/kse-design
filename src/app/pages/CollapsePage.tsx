e3ce915910d9275d078418371af42a5fa9e57a43
Oleinikov, Bogdan Konstantinovich <boleinikov@gmail.com>
2023-02-02 16:56:53 +0900

    no message

--- src/app/pages/CollapsePage.tsx
+++ src/app/pages/CollapsePage.tsx
@@ -251,6 +251,503 @@ export const CollapsePage = () => {
           </div>
         </div>
       </div>
+
+      <h3>info type</h3>
+      <div style={{ maxWidth: 600 }} className="kse-collapse-info-wrapper">
+        <div className="kse-collapse ">
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                실버
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                골드
+              </div>
+            </div>
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                금화
+              </div>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                골드바
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                공예품{" "}
+              </div>
+            </div>
+
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은장식품{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은계급장{" "}
+                    </div>
+                  </div>
+
+                  <div
+                    className="kse-collapse-content kse-list "
+                    style={{ display: "block" }}
+                  >
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      군인 계급장
+                    </div>
+
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      경찰 계급장
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      그라폰 파버카스텔
+                    </div>
+                  </div>
+
+                  <div className="kse-collapse-content kse-list ">
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      필기구
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                아트투자{" "}
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                수집용품
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h3>warning type</h3>
+      <div style={{ maxWidth: 600 }} className="kse-collapse-warning-wrapper">
+        <div className="kse-collapse ">
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                실버
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                골드
+              </div>
+            </div>
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                금화
+              </div>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                골드바
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                공예품{" "}
+              </div>
+            </div>
+
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은장식품{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은계급장{" "}
+                    </div>
+                  </div>
+
+                  <div
+                    className="kse-collapse-content kse-list "
+                    style={{ display: "block" }}
+                  >
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      군인 계급장
+                    </div>
+
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      경찰 계급장
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      그라폰 파버카스텔
+                    </div>
+                  </div>
+
+                  <div className="kse-collapse-content kse-list ">
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      필기구
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                아트투자{" "}
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                수집용품
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h3>success type</h3>
+      <div style={{ maxWidth: 600 }} className="kse-collapse-success-wrapper">
+        <div className="kse-collapse ">
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                실버
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                골드
+              </div>
+            </div>
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                금화
+              </div>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                골드바
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                공예품{" "}
+              </div>
+            </div>
+
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은장식품{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은계급장{" "}
+                    </div>
+                  </div>
+
+                  <div
+                    className="kse-collapse-content kse-list "
+                    style={{ display: "block" }}
+                  >
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      군인 계급장
+                    </div>
+
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      경찰 계급장
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      그라폰 파버카스텔
+                    </div>
+                  </div>
+
+                  <div className="kse-collapse-content kse-list ">
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      필기구
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                아트투자{" "}
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                수집용품
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h3>danger type</h3>
+      <div style={{ maxWidth: 600 }} className="kse-collapse-danger-wrapper">
+        <div className="kse-collapse ">
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                실버
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                골드
+              </div>
+            </div>
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                금화
+              </div>
+              <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                골드바
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                공예품{" "}
+              </div>
+            </div>
+
+            <div className="kse-collapse-content" style={{ display: "block" }}>
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은장식품{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      은계급장{" "}
+                    </div>
+                  </div>
+
+                  <div
+                    className="kse-collapse-content kse-list "
+                    style={{ display: "block" }}
+                  >
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      군인 계급장
+                    </div>
+
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      경찰 계급장
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" kse-collapse">
+                <div className="kse-collapse-panel">
+                  <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+                    <div
+                      style={{ paddingLeft: 16 }}
+                      className="kse-collapse-header-text"
+                    >
+                      그라폰 파버카스텔
+                    </div>
+                  </div>
+
+                  <div className="kse-collapse-content kse-list ">
+                    <div className="kse-text-middle kse-link-hover kse-collapse-link-text">
+                      필기구
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                아트투자{" "}
+              </div>
+            </div>
+          </div>
+          <div className="kse-collapse-panel ">
+            <div className=" kse-collapse-header kse-collapse-header-large  kse-space kse-space-middle">
+              <div
+                style={{ paddingLeft: 16 }}
+                className="kse-collapse-header-text kse-text-middle"
+              >
+                수집용품
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h2>Inspirations</h2>
       <a href="https://ant.design/components/collapse">Antd Collapse </a>
     </>
