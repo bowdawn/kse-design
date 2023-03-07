a8b0483176fef2d5f39be59d09d0e78ba7e18624
Oleinikov, Bogdan Konstantinovich <boleinikov@gmail.com>
2023-03-07 14:00:34 +0900

    edit

--- src/app/pages/ScreenMobilePage.tsx
+++ src/app/pages/ScreenMobilePage.tsx
@@ -7,6 +7,8 @@ export const ScreenMobilePage: React.FC = () => {
       <a href="https://www.koreasilverex.co.kr/m/main.html">
         1. https://www.koreasilverex.co.kr/m/main.html
       </a>
+      <h1>Login Page</h1>
+      <a href="https://www.koreasilverex.co.kr/m/login.html">2. https://www.koreasilverex.co.kr/m/login.html</a>
     </>
   );
 };
